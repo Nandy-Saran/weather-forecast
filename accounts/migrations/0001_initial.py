7a4aa7733ff40f4e18c2b14b2d3efa1d59ca59bd
Hari Prasath S <hariprasathhari9292@gmail.com>
2018-03-23 21:15:23 +0530

    error branch

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -8,6 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('datamodel', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
@@ -16,18 +17,28 @@ class Migration(migrations.Migration):
             name='Profile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('bio', models.TextField(max_length=500, blank=True)),
+                ('location', models.CharField(max_length=30, blank=True)),
+                ('birth_date', models.DateField(blank=True, null=True)),
+                ('email_confirmed', models.BooleanField(default=False)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Subscriber',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('name', models.CharField(max_length=100, blank=True)),
                 ('Mobile_no', models.CharField(max_length=15)),
                 ('land_ha', models.FloatField(max_length=15, blank=True, null=True)),
-                ('soil_type', models.CharField(max_length=15, blank=True)),
-                ('soil_ph', models.FloatField(max_length=10, blank=True)),
-                ('district', models.CharField(max_length=10, blank=True)),
-                ('sub_district', models.CharField(max_length=10, blank=True)),
-                ('category', models.CharField(max_length=10, blank=True)),
-                ('yield_tons', models.FloatField()),
-                ('crop1', models.CharField(max_length=30, blank=True)),
-                ('crop2', models.CharField(max_length=30, blank=True)),
-                ('email_confirmed', models.BooleanField(default=False)),
+                ('soil_type', models.CharField(max_length=15, blank=True, null=True)),
+                ('soil_ph', models.FloatField(max_length=10, blank=True, null=True)),
+                ('district', models.CharField(max_length=10, blank=True, null=True)),
+                ('category', models.CharField(max_length=10, blank=True, null=True)),
+                ('yield_tons', models.FloatField(null=True)),
+                ('crop1', models.ForeignKey(blank=True, null=True, related_name='crop_2_name', to='datamodel.Crop')),
+                ('crop2', models.ForeignKey(blank=True, null=True, related_name='crop_1_Name', to='datamodel.Crop')),
+                ('location', models.ForeignKey(to='datamodel.Place')),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from datamodel.models import Crop, Place
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -14,7 +16,7 @@ class Profile(models.Model):
     email_confirmed = models.BooleanField(default=False)
 
 
-class Subsciber(models.Model):
+class Subscriber(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True)
     Mobile_no = models.CharField(max_length=15)
@@ -22,11 +24,14 @@ class Subsciber(models.Model):
     soil_type = models.CharField(max_length=15, blank=True, null=True)
     soil_ph = models.FloatField(max_length=10, blank=True, null=True)
     district = models.CharField(max_length=10, blank=True, null=True)
-    sub_district = models.CharField(max_length=10, blank=True, null=True)
+    location = models.ForeignKey(Place)
     category = models.CharField(max_length=10, blank=True, null=True)
     yield_tons = models.FloatField(null=True)
-    crop1 = models.CharField(max_length=30, blank=True, null=True)
-    crop2 = models.CharField(max_length=30, blank=True, null=True)
+    crop1 = models.ForeignKey(Crop, related_name='crop_2_name', blank=True, null=True)
+    crop2 = models.ForeignKey(Crop, related_name='crop_1_Name', blank=True, null=True)
+
+
+
 
 
 @receiver(post_save, sender=User)
